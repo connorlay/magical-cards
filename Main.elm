07b062f765a5c98232dd5cbccc1601cb37a4f819
Maggie Dreyer <maggie@puppet.com>
2016-11-12 15:51:07 -0800

    Hello world with Elm in Electron

---
+++ Main.elm
@@ -0,0 +1,18 @@
+port module Mtg exposing (..)
+import Html exposing (Html, text)
+import Html.App as App
+
+main = text "Hello"
+
+{-main =
+  App.beginnerProgram
+    { model = 0
+    , view = view
+    , update = \_ -> \_ -> 0
+    }
+
+type Msg = Increment | Decrement
+
+view : Int -> Html Msg
+view model = text "hello"-}
+
