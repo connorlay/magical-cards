d92fd86bbcdfaee833cbb7944b938fe31b494d1a
Connor Lay <connor.lay@me.com>
2016-12-03 16:30:17 -0800

    Add remove by predicate

--- src/Library/Library.elm
+++ src/Library/Library.elm
@@ -29,3 +29,21 @@ pushTop card library =
 pushBottom : Card -> Library -> Library
 pushBottom card library =
     library ++ [ card ]
+
+
+removeByName : String -> Library -> ( Maybe Card, Library )
+removeByName name library =
+  remove (\card -> card.name == name) [] library
+
+
+remove : ( a -> Bool ) -> List a -> List a -> ( Maybe a, List a )
+remove predicate before after =
+  case after of
+    [] ->
+        ( Nothing, before )
+
+    x :: xs ->
+      if predicate x then
+        ( Just x, before ++ xs )
+      else
+        remove predicate (before ++ [ x ]) xs
--- tests/Tests.elm
+++ tests/Tests.elm
@@ -16,17 +16,37 @@ library : Test
 library =
     describe "Library module"
         [ describe "removing a card"
-            [ test "from the top" <|
-                \() ->
-                    let
-                        library =
-                            [ Card "a", Card "b", Card "c" ]
-                    in
-                        Expect.equal ( Just (Card "a"), [ Card "b", Card "c" ] ) <|
-                            pop library
-            , test "from an empty library" <|
-                \() ->
-                    Expect.equal ( Nothing, [] ) <| pop []
+            [ describe "from the top"
+                [ test "non-empty library" <|
+                    \() ->
+                        let
+                            library =
+                                [ Card "a", Card "b", Card "c" ]
+                        in
+                            Expect.equal ( Just (Card "a"), [ Card "b", Card "c" ] ) <|
+                                pop library
+                , test "empty library" <|
+                    \() ->
+                        Expect.equal ( Nothing, [] ) <| pop []
+                ]
+            , describe "by name" <|
+                [ test "card is present" <|
+                    \() ->
+                        let
+                            library =
+                                [ Card "a", Card "b", Card "c" ]
+                        in
+                            Expect.equal ( Just (Card "b"), [ Card "a", Card "c" ] ) <|
+                                removeByName "b" library
+                , test "card is not present" <|
+                    \() ->
+                        let
+                            library =
+                                [ Card "a", Card "b" ]
+                        in
+                            Expect.equal ( Nothing, [ Card "a", Card "b" ] ) <|
+                                removeByName "c" library
+                ]
             ]
         , describe "adding a card"
             [ test "to the top" <|
@@ -38,11 +58,12 @@ library =
                         Expect.equal (Card "d" :: library) <|
                             pushTop (Card "d") library
             , test "to the bottom" <|
-                let
-                    library =
-                        [ Card "a", Card "b", Card "c" ]
-                in
-                    Expect.equal (Card "d" :: library) <|
-                        pushTop (Card "d") library
+                \() ->
+                    let
+                        library =
+                            [ Card "a", Card "b", Card "c" ]
+                    in
+                        Expect.equal (library ++ [ Card "d" ]) <|
+                            pushBottom (Card "d") library
             ]
         ]
